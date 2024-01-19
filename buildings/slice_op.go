4454f6228bd2ab1776c3e19d6bd2557c878722bb
Kozlov Egor Alexandrovich <PublicDevelMail@yandex.ru>
2024-01-19 07:36:50 +0300

    Исправление коментария в `slice_op.go`.

--- buildings/slice_op.go
+++ buildings/slice_op.go
@@ -42,7 +42,7 @@ func TryAddUniqueInstance(collection *[]IBuilding, instance IBuilding) bool {
 	for _, v := range *collection {
 		// Сравниваем значения попарно,
 		// т.к.  интерфейс - указатель и оператор == сравнивает адреса
-		// Проверку country упкскаем, т.к. country зависит от проверяемых значений
+		// Проверку class упкскаем, т.к. class зависит от проверяемых значений
 		if v.GetAddress() == instance.GetAddress() && v.GetAge() == instance.GetAge() {
 			// Нашли такую-же структуру - выходим.
 			return false
